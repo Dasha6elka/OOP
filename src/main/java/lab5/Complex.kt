63fac0c8feb17d947e53777e234ed8e9013198d5
Dasha6elka <CB4kot@gmail.com>
2019-04-03 21:11:50 +0300

    рефакторинг Complex

--- src/main/java/lab5/Complex.kt
+++ src/main/java/lab5/Complex.kt
@@ -1,37 +1,30 @@
 package lab5
 
+import org.apache.commons.lang3.math.NumberUtils
+import java.io.IOException
+import java.io.InputStream
+import java.io.OutputStream
+
 class Complex constructor(private var real: Double = 0.0, private var image: Double = 0.0) {
-    fun re(): Double {
-        return real
+    companion object {
+        const val EPS = 10e-15
     }
 
-    fun im(): Double {
-        return image
-    }
+    fun re(): Double = real
 
-    fun getMagnitude(): Double {
-        return Math.sqrt(Math.pow(real, 2.0) + Math.pow(image, 2.0))
-    }
+    fun im(): Double = image
 
-    fun getArgument(): Double {
-        return Math.atan(image/real)
-    }
+    fun getMagnitude(): Double = Math.sqrt(Math.pow(real, 2.0) + Math.pow(image, 2.0))
 
-    operator fun plus(number: Complex): Complex {
-        return Complex(real + number.re(), image + number.im())
-    }
+    fun getArgument(): Double = Math.atan2(image, real)
 
-    operator fun plus(number: Double): Complex {
-        return Complex(real + number, image)
-    }
+    operator fun plus(number: Complex): Complex = Complex(real + number.re(), image + number.im())
 
-    operator fun minus(number: Complex): Complex {
-        return Complex(real - number.re(), image - number.im())
-    }
+    operator fun plus(number: Double): Complex = Complex(real + number, image)
 
-    operator fun minus(number: Double): Complex {
-        return Complex(real - number, image)
-    }
+    operator fun minus(number: Complex): Complex = Complex(real - number.re(), image - number.im())
+
+    operator fun minus(number: Double): Complex = Complex(real - number, image)
 
     operator fun times(number: Complex): Complex {
         val factor1 = (real * number.re()) - (image * number.im())
@@ -39,11 +32,144 @@ class Complex constructor(private var real: Double = 0.0, private var image: Dou
         return Complex(factor1, factor2)
     }
 
-    operator fun times(number: Double): Complex {
-        return Complex(real * number, image * number)
+    operator fun times(number: Double): Complex = this * Complex(number)
+
+    operator fun div(number: Complex): Complex {
+        val bottom = Math.pow(number.re(), 2.0) + Math.pow(number.im(), 2.0)
+        val factor1 = ((real * number.re()) + (image * number.im())) / bottom
+        val factor2 = ((image * number.re()) - (real * number.im())) / bottom
+        return Complex(factor1, factor2)
     }
+
+    operator fun div(number: Double): Complex = this / Complex(number)
 
     operator fun unaryPlus() = Complex(real, image)
 
     operator fun unaryMinus() = Complex(-real, -image)
+
+    operator fun plusAssign(number: Complex) {
+        val complex = this + number
+        real = complex.re()
+        image = complex.im()
+    }
+
+    operator fun plusAssign(number: Double) {
+        this += Complex(number)
+    }
+
+    operator fun minusAssign(number: Complex) {
+        val complex = this - number
+        real = complex.re()
+        image = complex.im()
+    }
+
+    operator fun minusAssign(number: Double) {
+        this -= Complex(number)
+    }
+
+    operator fun timesAssign(number: Complex) {
+        val complex = this * number
+        real = complex.re()
+        image = complex.im()
+    }
+
+    operator fun timesAssign(number: Double) {
+        val complex = this * number
+        real = complex.re()
+        image = complex.im()
+    }
+
+    operator fun divAssign(number: Complex) {
+        val complex = this / number
+        real = complex.re()
+        image = complex.im()
+    }
+
+    operator fun divAssign(number: Double) {
+        val complex = this / number
+        real = complex.re()
+        image = complex.im()
+    }
+
+    override operator fun equals(other: Any?): Boolean {
+        return when (other) {
+            is Complex -> eq(real, other.re()) && eq(image, other.im())
+            is Double -> {
+                val complex = Complex(other)
+                return eq(real, complex.re()) && eq(image, complex.im())
+            }
+            else -> return false
+        }
+    }
+
+    private fun eq(left: Double, right: Double): Boolean {
+        return Math.abs(left - right) < EPS
+    }
+
+    override fun hashCode(): Int {
+        var result = real.hashCode()
+        result = 31 * result + image.hashCode()
+        return result
+    }
+}
+
+operator fun Double.plus(other: Complex): Complex {
+    return Complex(this + other.re(), other.im())
+}
+
+operator fun Double.minus(other: Complex): Complex {
+    return Complex(this - other.re(), other.im())
+}
+
+operator fun Double.times(other: Complex): Complex {
+    return Complex(this * other.re(), other.im())
+}
+
+operator fun Double.div(other: Complex): Complex {
+    return Complex(this / other.re(), other.im())
+}
+
+fun OutputStream.write(complex: Complex) {
+    val sign = if (complex.im() < 0) '-' else '+'
+    val str = "${complex.re()}$sign${Math.abs(complex.im())}i"
+    write(str.toByteArray())
+}
+
+fun InputStream.read(complex: Complex) {
+    val plus = '+'.toInt()
+    val minus = '-'.toInt()
+    val suffix = 'i'.toInt()
+
+    var input = read()
+    val unary =
+        if (input == minus) {
+            input = read()
+            '-'
+        } else '+'
+
+    var re = ""
+    while (input != plus && input != minus) {
+        re += input.toChar()
+        input = read()
+    }
+
+    val sign = input.toChar()
+    input = read()
+
+    var im = ""
+    while (input != suffix && input != -1) {
+        im += input.toChar()
+        input = read()
+    }
+
+    if (!NumberUtils.isCreatable(re)) {
+        throw IOException("Something is wrong with real part!")
+    }
+    if (!NumberUtils.isCreatable(im)) {
+        throw IOException("Something is wrong with image part!")
+    }
+
+    val real = NumberUtils.toDouble(re)
+    val image = NumberUtils.toDouble(im)
+    complex += Complex(if (unary == '-') -real else real, if (sign == '-') -image else image)
 }
--- src/test/java/lab5/ComplexTest.kt
+++ src/test/java/lab5/ComplexTest.kt
@@ -1,32 +1,96 @@
 package lab5
 
+import lab5.Complex.Companion.EPS
 import org.junit.Test
 
 import org.junit.Assert.*
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+import java.io.PrintStream
 
 class ComplexTest {
     @Test
     fun `check re is correct`() {
         val complex = Complex(5.0, 10.0)
-        assertEquals(5.0, complex.re(), 0.0)
+        assertEquals(5.0, complex.re(), EPS)
     }
 
     @Test
     fun `check im is correct`() {
         val complex = Complex(5.0, 10.0)
-        assertEquals(10.0, complex.im(), 0.0)
+        assertEquals(10.0, complex.im(), EPS)
     }
 
     @Test
     fun `check getMagnitude is correct`() {
         val complex = Complex(5.0, 10.0)
-        assertEquals(11.180339887498949, complex.getMagnitude(), 0.0)
+        assertEquals(11.180339887498949, complex.getMagnitude(), EPS)
+    }
+
+    @Test
+    fun `check getMagnitude with zero is correct`() {
+        val complex = Complex(0.0, 0.0)
+        assertEquals(0.0, complex.getMagnitude(), EPS)
+    }
+
+    @Test
+    fun `check getMagnitude with ones is zero is correct`() {
+        val complex = Complex(0.0, 13.0)
+        assertEquals(13.0, complex.getMagnitude(), EPS)
+    }
+
+    @Test
+    fun `check getMagnitude with negative number is correct`() {
+        val complex = Complex(-5.0, -2.0)
+        assertEquals(5.385164807134504, complex.getMagnitude(), EPS)
+    }
+
+    @Test
+    fun `check getMagnitude with ones is negative is correct`() {
+        val complex = Complex(0.0, -13.0)
+        assertEquals(13.0, complex.getMagnitude(), EPS)
     }
 
     @Test
     fun `check getArgument is correct`() {
         val complex = Complex(5.0, 10.0)
-        assertEquals(1.1071487177940904, complex.getArgument(), 0.0)
+        assertEquals(1.1071487177940904, complex.getArgument(), EPS)
+    }
+
+    @Test
+    fun `check getArgument with real is negative is correct`() {
+        val complex = Complex(-5.0, 10.0)
+        assertEquals(2.0344439357957027, complex.getArgument(), EPS)
+    }
+
+    @Test
+    fun `check getArgument with image is negative is correct`() {
+        val complex = Complex(5.0, -10.0)
+        assertEquals(-1.1071487177940904, complex.getArgument(), EPS)
+    }
+
+    @Test
+    fun `check getArgument with negative is correct`() {
+        val complex = Complex(-5.0, -10.0)
+        assertEquals(-2.0344439357957027, complex.getArgument(), EPS)
+    }
+
+    @Test
+    fun `check getArgument with real is zero is correct`() {
+        val complex = Complex(0.0, 10.0)
+        assertEquals(1.5707963267948966, complex.getArgument(), EPS)
+    }
+
+    @Test
+    fun `check getArgument with image is zero is correct`() {
+        val complex = Complex(10.0, 0.0)
+        assertEquals(0.0, complex.getArgument(), EPS)
+    }
+
+    @Test
+    fun `check getArgument with zero is correct`() {
+        val complex = Complex(0.0, 0.0)
+        assertEquals(0.0, complex.getArgument(), EPS)
     }
 
     @Test
@@ -35,8 +99,8 @@ class ComplexTest {
         val complex2 = Complex(7.0, 6.0)
         val result = complex1 + complex2
         val resultRight = Complex(12.0, 16.0)
-        assertEquals(resultRight.re(), result.re(), 0.0)
-        assertEquals(resultRight.im(), result.im(), 0.0)
+        assertEquals(resultRight.re(), result.re(), EPS)
+        assertEquals(resultRight.im(), result.im(), EPS)
     }
 
     @Test
@@ -44,8 +108,8 @@ class ComplexTest {
         val complex1 = Complex(5.0, 10.0)
         val result = complex1 + 18.0
         val resultRight = Complex(23.0, 10.0)
-        assertEquals(resultRight.re(), result.re(), 0.0)
-        assertEquals(resultRight.im(), result.im(), 0.0)
+        assertEquals(resultRight.re(), result.re(), EPS)
+        assertEquals(resultRight.im(), result.im(), EPS)
     }
 
     @Test
@@ -54,8 +118,8 @@ class ComplexTest {
         val complex2 = Complex(7.0, 6.0)
         val result = complex1 - complex2
         val resultRight = Complex(-2.0, 4.0)
-        assertEquals(resultRight.re(), result.re(), 0.0)
-        assertEquals(resultRight.im(), result.im(), 0.0)
+        assertEquals(resultRight.re(), result.re(), EPS)
+        assertEquals(resultRight.im(), result.im(), EPS)
     }
 
     @Test
@@ -63,8 +127,8 @@ class ComplexTest {
         val complex1 = Complex(5.0, 10.0)
         val result = complex1 - 25.0
         val resultRight = Complex(-20.0, 10.0)
-        assertEquals(resultRight.re(), result.re(), 0.0)
-        assertEquals(resultRight.im(), result.im(), 0.0)
+        assertEquals(resultRight.re(), result.re(), EPS)
+        assertEquals(resultRight.im(), result.im(), EPS)
     }
 
     @Test
@@ -73,8 +137,8 @@ class ComplexTest {
         val complex2 = Complex(7.0, 6.0)
         val result = complex1 * complex2
         val resultRight = Complex(-25.0, 100.0)
-        assertEquals(resultRight.re(), result.re(), 0.0)
-        assertEquals(resultRight.im(), result.im(), 0.0)
+        assertEquals(resultRight.re(), result.re(), EPS)
+        assertEquals(resultRight.im(), result.im(), EPS)
     }
 
     @Test
@@ -82,8 +146,27 @@ class ComplexTest {
         val complex1 = Complex(5.0, 10.0)
         val result = complex1 * 2.0
         val resultRight = Complex(10.0, 20.0)
-        assertEquals(resultRight.re(), result.re(), 0.0)
-        assertEquals(resultRight.im(), result.im(), 0.0)
+        assertEquals(resultRight.re(), result.re(), EPS)
+        assertEquals(resultRight.im(), result.im(), EPS)
+    }
+
+    @Test
+    fun `check operator div with Complex number is correct`() {
+        val complex1 = Complex(20.0, 10.0)
+        val complex2 = Complex(4.0, 2.0)
+        val result = complex1 / complex2
+        val resultRight = Complex(5.0, 0.0)
+        assertEquals(resultRight.re(), result.re(), EPS)
+        assertEquals(resultRight.im(), result.im(), EPS)
+    }
+
+    @Test
+    fun `check operator div with Double number is correct`() {
+        val complex1 = Complex(5.0, 10.0)
+        val result = complex1 / 2.0
+        val resultRight = Complex(2.5, 5.0)
+        assertEquals(resultRight.re(), result.re(), EPS)
+        assertEquals(resultRight.im(), result.im(), EPS)
     }
 
     @Test
@@ -91,8 +174,8 @@ class ComplexTest {
         val complex1 = Complex(5.0, 10.0)
         val result = +complex1
         val resultRight = Complex(5.0, 10.0)
-        assertEquals(resultRight.re(), result.re(), 0.0)
-        assertEquals(resultRight.im(), result.im(), 0.0)
+        assertEquals(resultRight.re(), result.re(), EPS)
+        assertEquals(resultRight.im(), result.im(), EPS)
     }
 
     @Test
@@ -100,7 +183,218 @@ class ComplexTest {
         val complex1 = Complex(5.0, 10.0)
         val result = -complex1
         val resultRight = Complex(-5.0, -10.0)
-        assertEquals(resultRight.re(), result.re(), 0.0)
-        assertEquals(resultRight.im(), result.im(), 0.0)
+        assertEquals(resultRight.re(), result.re(), EPS)
+        assertEquals(resultRight.im(), result.im(), EPS)
+    }
+
+    @Test
+    fun `check operator plusAssign with Complex number is correct`() {
+        val complex1 = Complex(20.0, 10.0)
+        val complex2 = Complex(4.0, 2.0)
+        complex1 += complex2
+        val resultRight = Complex(24.0, 12.0)
+        assertEquals(resultRight.re(), complex1.re(), EPS)
+        assertEquals(resultRight.im(), complex1.im(), EPS)
+    }
+
+    @Test
+    fun `check operator plusAssign with Double number is correct`() {
+        val complex1 = Complex(5.0, 10.0)
+        complex1 += 2.0
+        val resultRight = Complex(7.0, 10.0)
+        assertEquals(resultRight.re(), complex1.re(), EPS)
+        assertEquals(resultRight.im(), complex1.im(), EPS)
+    }
+
+    @Test
+    fun `check operator minusAssign with Complex number is correct`() {
+        val complex1 = Complex(20.0, 10.0)
+        val complex2 = Complex(4.0, 2.0)
+        complex1 -= complex2
+        val resultRight = Complex(16.0, 8.0)
+        assertEquals(resultRight.re(), complex1.re(), EPS)
+        assertEquals(resultRight.im(), complex1.im(), EPS)
+    }
+
+    @Test
+    fun `check operator minusAssign with Double number is correct`() {
+        val complex1 = Complex(5.0, 10.0)
+        complex1 -= 2.0
+        val resultRight = Complex(3.0, 10.0)
+        assertEquals(resultRight.re(), complex1.re(), EPS)
+        assertEquals(resultRight.im(), complex1.im(), EPS)
+    }
+
+    @Test
+    fun `check operator timesAssign with Complex number is correct`() {
+        val complex1 = Complex(20.0, 10.0)
+        val complex2 = Complex(4.0, 2.0)
+        complex1 *= complex2
+        val resultRight = Complex(60.0, 80.0)
+        assertEquals(resultRight.re(), complex1.re(), EPS)
+        assertEquals(resultRight.im(), complex1.im(), EPS)
+    }
+
+    @Test
+    fun `check operator timesAssign with Double number is correct`() {
+        val complex1 = Complex(5.0, 10.0)
+        complex1 *= 2.0
+        val resultRight = Complex(10.0, 20.0)
+        assertEquals(resultRight.re(), complex1.re(), EPS)
+        assertEquals(resultRight.im(), complex1.im(), EPS)
+    }
+
+    @Test
+    fun `check operator divAssign with Complex number is correct`() {
+        val complex1 = Complex(20.0, 10.0)
+        val complex2 = Complex(4.0, 2.0)
+        complex1 /= complex2
+        val resultRight = Complex(5.0, 0.0)
+        assertEquals(resultRight.re(), complex1.re(), EPS)
+        assertEquals(resultRight.im(), complex1.im(), EPS)
+    }
+
+    @Test
+    fun `check operator divAssign with Double number is correct`() {
+        val complex1 = Complex(5.0, 10.0)
+        complex1 /= 2.0
+        val resultRight = Complex(2.5, 5.0)
+        assertEquals(resultRight.re(), complex1.re(), EPS)
+        assertEquals(resultRight.im(), complex1.im(), EPS)
+    }
+
+    @Test
+    fun `check operator equals with Complex numbers is correct`() {
+        val complex1 = Complex(20.0, 10.0)
+        val complex2 = Complex(20.0, 10.0)
+        assertTrue(complex1 == complex2)
+        assertFalse(complex1 != complex2)
+    }
+
+    @Test
+    fun `check operator not equals with Complex numbers is correct`() {
+        val complex1 = Complex(20.0, 10.0)
+        val complex2 = Complex(28.0, 14.0)
+        assertTrue(complex1 != complex2)
+        assertFalse(complex1 == complex2)
+    }
+
+    @Test
+    fun `check operator equals with Complex and Double numbers is correct`() {
+        val complex1 = Complex(20.0, 0.0)
+        val number = 20.0
+        assertTrue(complex1.equals(number))
+        assertFalse(!complex1.equals(number))
+    }
+
+    @Test
+    fun `check operator not equals with Complex and Double numbers is correct`() {
+        val complex1 = Complex(2.0, 0.0)
+        val number = 20.0
+        assertTrue(!complex1.equals(number))
+        assertFalse(complex1.equals(number))
+    }
+
+    private val myOut = ByteArrayOutputStream()
+    @Test
+    fun `check fun write with positive is correct`() {
+        System.setOut(PrintStream(myOut))
+        val complex = Complex(3.0, 4.0)
+        System.out.write(complex)
+        val right = "3.0+4.0i"
+        assertEquals(right, myOut.toString())
+    }
+
+    @Test
+    fun `check fun write with negative is correct`() {
+        System.setOut(PrintStream(myOut))
+        val complex = Complex(-3.0, -4.0)
+        System.out.write(complex)
+        val right = "-3.0-4.0i"
+        assertEquals(right, myOut.toString())
+    }
+
+    @Test
+    fun `check fun read with Complex positive is correct`() {
+        val input = "3+4i"
+        val `in` = ByteArrayInputStream(input.toByteArray())
+        System.setIn(`in`)
+        val complex = Complex()
+        `in`.read(complex)
+        val right = Complex(3.0, 4.0)
+        assertEquals(right, complex)
+    }
+
+    @Test
+    fun `check fun read with Complex negative is correct`() {
+        val input = "-3-4i"
+        val `in` = ByteArrayInputStream(input.toByteArray())
+        System.setIn(`in`)
+        val complex = Complex()
+        `in`.read(complex)
+        val right = Complex(-3.0, -4.0)
+        assertEquals(right, complex)
+    }
+
+    @Test
+    fun `check fun read with Double Complex positive is correct`() {
+        val input = "3.3+4.4i"
+        val `in` = ByteArrayInputStream(input.toByteArray())
+        System.setIn(`in`)
+        val complex = Complex()
+        `in`.read(complex)
+        val right = Complex(3.3, 4.4)
+        assertEquals(right, complex)
+    }
+
+    @Test
+    fun `check fun read with Double Complex negative is correct`() {
+        val input = "-3.3-4.4i"
+        val `in` = ByteArrayInputStream(input.toByteArray())
+        System.setIn(`in`)
+        val complex = Complex()
+        `in`.read(complex)
+        val right = Complex(-3.3, -4.4)
+        assertEquals(right, complex)
+    }
+
+    @Test
+    fun `check fun Double plus Complex is correct`() {
+        val double = 2.0
+        val complex = Complex(3.0, 6.0)
+        val rightNumber = Complex(5.0, 6.0)
+        val number = double + complex
+        assertEquals(rightNumber.re(), number.re(), EPS)
+        assertEquals(rightNumber.im(), number.im(), EPS)
+    }
+
+    @Test
+    fun `check fun Double minus Complex is correct`() {
+        val double = 2.0
+        val complex = Complex(3.0, 6.0)
+        val rightNumber = Complex(-1.0, 6.0)
+        val number = double - complex
+        assertEquals(rightNumber.re(), number.re(), EPS)
+        assertEquals(rightNumber.im(), number.im(), EPS)
+    }
+
+    @Test
+    fun `check fun Double times Complex is correct`() {
+        val double = 2.0
+        val complex = Complex(3.0, 6.0)
+        val rightNumber = Complex(6.0, 6.0)
+        val number = double * complex
+        assertEquals(rightNumber.re(), number.re(), EPS)
+        assertEquals(rightNumber.im(), number.im(), EPS)
+    }
+
+    @Test
+    fun `check fun Double div Complex is correct`() {
+        val double = 2.0
+        val complex = Complex(2.0, 6.0)
+        val rightNumber = Complex(1.0, 6.0)
+        val number = double / complex
+        assertEquals(rightNumber.re(), number.re(), EPS)
+        assertEquals(rightNumber.im(), number.im(), EPS)
     }
 }
